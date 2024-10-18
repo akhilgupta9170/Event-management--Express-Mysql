c28c7d5c25201be9c22a630bd782d4b73862cb52
Akhil gupta <akhilgupta18ald@gmail.com>
2024-10-18 16:59:35 +0530

    signup and login function is defined

---
+++ controller/authController.js
@@ -0,0 +1,33 @@
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const User = require("../model/userModel.js");
+
+exports.signup = async (req, res) => {
+  const { name, email, password, role } = req.body;
+  const hashedPassword = await bcrypt.hash(password, 10);
+  try {
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+      role,
+    });
+    res.status(201).json({ user });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+exports.login = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ where: { email } });
+  if (!user || bcrypt.compare(password, user.password)) {
+    return res.status(401).json({ error: "Invalid email or password" });
+  }
+  const token = jwt.sign(
+    { id: user.id, email: user.email, role: user.role },
+    process.env.JWT_SECRET,
+    { expiresIn: "1h" }
+  );
+  res.json({ token });
+};
