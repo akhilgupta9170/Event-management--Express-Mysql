b5c9b5b861b511640584fe1b1983b2fc0ceeb6db
Akhil gupta <akhilgupta18ald@gmail.com>
2024-10-18 17:29:25 +0530

    crud implemented in event controller for accesing the events

---
+++ controller/eventController.js
@@ -0,0 +1,53 @@
+const { Event } = require("../model/eventModel.js");
+
+exports.createEvent = async (req, res) => {
+  const { title, description, event_date, location } = req.body;
+  try {
+    const event = await Event.create({
+      title,
+      description,
+      event_date,
+      location,
+    });
+    res.status(201).json({ event });
+  } catch {
+    res.status(404).json({ error: error.message });
+  }
+};
+
+exports.getAllEvents = async (req, res) => {
+  try {
+    const events = await Event.findAll();
+    res.status(200).json({ events });
+  } catch (error) {
+    res.status(404).json({ error: error.message });
+  }
+};
+
+exports.creatEventById = async(req,res)=>{
+    const {id }= req.params;
+    const {title,description, event_date, location} = req.body;
+    try{
+    const user = await Event.findByPk(id);
+    if(!user){
+        return res.status(404).json({ error: 'Event not found' });
+    }
+    await user.update({title,description, event_date, location});
+    }
+    catch(error){
+        res.status(404).json({ error: error.message });
+    }
+}
+
+exports.deleteEventById = async(req,res)=>{
+    const {id} = req.params
+    try{
+        const user = await Event.findByPk(id);
+        if(!user)
+            return res.status(404).json({ error: 'Event not found' });
+        await user.destroy();
+    }
+    catch(error){
+        res.status(404).json({ error: error.message });
+    }
+}
